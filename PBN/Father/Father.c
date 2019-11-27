53c7b14e3130be8b824233d67cff80212fe9ea18
JoacoPBN <58235706+JoacoPBN@users.noreply.github.com>
2019-11-27 11:16:20 -0300

    Father

--- PBN/Father/Father.c
+++ PBN/Father/Father.c
@@ -6,17 +6,23 @@ int main(){
 	//int puerto = atoi(argv[1]);
 
     Proceso listPro[100];
-    void *shm = crearSHM(DIR_KEY_SHM);
-    shm = listPro;
-    int id = generarID(DIR_KEY_SHM);
-//	int creacionSist[] = crearProcSis[] (puerto);
-    Proceso pro = {1232,"hefwef",5,"ddasdas"};
-    listPro[0] = pro;
-    eliminarSHM(id);
-    desconectarSHM(shm);
-    
-	
-	//Codigo para suspenderse	
+        void *shm = crearSHM(DIR_KEY_SHM);
+        Proceso *lista;
+        lista = (Proceso *)(shm + 50);
+        int id = generarID(DIR_KEY_SHM);
+    //    int creacionSist[] = crearProcSis[] (puerto);
+        Proceso pro = {1232,"hefwef",5,"ddasdas"};
+        Proceso pro1 = {1232,"hefwef",6,"ddasdas"};
+
+            lista[0] = pro;
+        lista[1]=pro1;
+
+        printf("\nPrintf: %d-%d\n",pro2.estado,lista[1].estado);
+        eliminarSHM(id);
+        desconectarSHM((void *)shm);
+        
+        
+        //Codigo para suspenderse
 
 
 	return EXIT_SUCCESS;
