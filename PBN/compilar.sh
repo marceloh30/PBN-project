f7e280347c5c4b57a8d08271a9a03ca6b5557cce
JoacoPBN <58235706+JoacoPBN@users.noreply.github.com>
2019-11-28 01:59:45 -0300

    Joaco 3

--- PBN/compilar.sh
+++ PBN/compilar.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-gcc -pthread -Wall GeneralFunctions.h GeneralFunctions.c Semaforo/Semaforo.h Semaforo/Semaforo.c SHM/SHMFun.h SHM/SHMFun.c ProcessManager/ProcessManagerFun.h ProcessManager/ProcessManagerFun.c ProcessManager/ProcessManager.c;
+gcc -pthread -Wall GeneralFunctions.h GeneralFunctions.c Semaforo/Semaforo.h Semaforo/Semaforo.c SHM/SHMFun.h SHM/SHMFun.c Sockets/SocketFunc.h Sockets/SocketFunc.c ProcessManager/ProcessManagerFun.h ProcessManager/ProcessManagerFun.c ProcessManager/ProcessManager.c;
 
-gcc -pthread -Wall Semaforo/Semaforo.c DynamicLists.c SHM/SHMFun.c Sockets/SocketFunc.c Actuador/AFun.c Actuador/Actuador.c;
+gcc -pthread -Wall Semaforo/Semaforo.h Semaforo/Semaforo.c DynamicLists.h DynamicLists.c SHM/SHMFun.h SHM/SHMFun.c Sockets/SocketFunc.h Sockets/SocketFunc.c Actuador/AFun.h Actuador/AFun.c Actuador/Actuador.h Actuador/Actuador.c;
 
-gcc 
+gcc -pthread -Wall GeneralFunctions.h GeneralFunctions.c Semaforo/Semaforo.h Semaforo/Semaforo.c SHM/SHMFun.h SHM/SHMFun.c Father/FatherFun.h Father/FatherFun.c Father/Father.c
+
+echo "\n\n\n\n\n\n\n\n" #Genera espacio entre cada corrida
